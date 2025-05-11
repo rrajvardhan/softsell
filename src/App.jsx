9079765ac20d10f965a3207c9e52bc64955ea86d
rrajvardhan <rajvardhantomar25@gmail.com>
2025-05-11 11:46:38 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import HeroSection from "./components/Hero"
 import Navbar from "./components/Navbar"
 import WhyChooseUs from "./components/WhyUs"
